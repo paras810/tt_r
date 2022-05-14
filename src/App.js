34672de81d305e4120c5c96a04efd88319987d74
html <newparas11422@gmail.com>
2022-05-14 10:34:08 +0530

    form

--- src/App.js
+++ src/App.js
@@ -4,38 +4,60 @@ import './App.css'
 
 
 const App = () => {
- const fullName = ['vinod', 'thapa']
- const bioData = [1, 24, 'male',...fullName]
-//  console.log(fullName);
-//  console.log(bioData);
+  const [fullName, setFullName] = useState(
+    {
+      fname:'',
+      lname: ''
+    }
+  )
 
-let [a,b,...c] =['a','b','c','d','e']
-let game2 =['f','g','h','i','j']
-// console.log(game1+ game2);
-// console.log(game1 , game2);
-// console.log(...game1 , ...game2);
-// console.log(a);
-// console.log(b);
-// console.log(c);
+const inputEvent=(e)=>{
+console.log(e.target.value);
+console.log(e.target.name);
 
-const fullNameObj ={
-  fname: 'paras',
-  lname: 'nothing'
+const value = e.target.value
+const name = e.target.name
+
+setFullName((preValue)=>{
+  // console.log(preValue);
+  if (name === 'fname') {
+   return{
+     fname:value,
+    lname: preValue.lname
+   }
+  }
+  
+  else if (name === 'lname') {
+    return{
+      fname: preValue.fname,
+     lname: value
+    }
+   }
+})
 }
-const bioObj ={
-  id: 1,
-  age: 24,
-  gender: 'male',
-  ...fullNameObj
 
+const onSumbit =(event)=>{
+  event.preventDefault()
+//  alert('form submitted')s
+ setFullName({
+  fname:'',
+  lname: ''
+ })
 }
 
-console.log(bioObj);
+
   return (
-    <div >
-     
+    <form className='div' onSubmit={onSumbit} >
 
-    </div>
+     <h1> {`${fullName.fname} ${fullName.lname}`} </h1>
+     <input type="text" placeholder='enter your first name'  onChange={inputEvent}  value={fullName.fname} name="fname"/>
+
+     <input type="text" placeholder='enter your last name'  onChange={inputEvent} value={fullName.lname}  name="lname"/>
+     <button type='submit'>Sumbit</button>
+
+
+
+    </form>
   )
 }
 
