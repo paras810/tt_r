301dc95cf8485d6f625ae3beeb0b2fd55b2e1eb6
html <newparas11422@gmail.com>
2022-05-16 04:47:44 +0530

    useLocation hook in react router

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Link , useParams} from 'react-router-dom'
+import { Link , useLocation, useParams} from 'react-router-dom'
 
 const User = () => {
     const {fname , lname} = useParams() 
+    const location = useLocation()
+    // console.log(location)
+    // console.log(location.pathname)
 
   return (
       <>
     <div> user page { fname } { lname }</div>
     <button><Link to="/">Home Page</Link></button>
+    <p>curren location is {location.pathname}</p>
+    {location.pathname === '/user/paras/added'? <button onClick={()=>{alert('going to service page')}}> <Link to="/service">More</Link></button>: null}
     </>
   )
 }
